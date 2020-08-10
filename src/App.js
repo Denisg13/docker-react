87f1da3ea03ace3a493b826e654c10905269e18d
Denisg13 <parazit13g@gmail.com>
2020-08-10 15:41:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Я поменял эту надпись !
+          
+        </p>
+        <a
+          className="App-link"
+          href="ВОВА СПАСИБО )))"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
